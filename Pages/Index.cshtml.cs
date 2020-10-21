19cfee680d0e72a3947a08d618895e778f09c1f5
q55mehqs <s1701057@sendai-nct.jp>
2020-10-21 12:51:49 +0900

    fix to holiday error and henko data

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SnctJikanwari;
+using SnctJikanwari.JikanwariContents;
 using SnctJikanwari.JikanwariContents.Jugyo;
 
 namespace SnctPageCore.Pages
@@ -8,10 +11,29 @@ namespace SnctPageCore.Pages
     public class IndexModel : PageModel
     {
         public IEnumerable<IJugyo> DayJugyos { get; private set; }
+        public string DayTitle { get; private set; }
+
         public async void OnGet()
         {
-            var (jugyo, _) = await SnctJikanwari.JikanwariContents.JikanwariManager.GetJikanwari("IS4", DateTime.Today);
+            var date = DateTime.Today.SkipHoliday();
+            var (jugyo, dayOfWeek) = await JikanwariManager.GetJikanwari("IS4", date);
+            DayTitle = date.ToString("yyyy/MM/dd (ddd)", new CultureInfo("ja-JP")) + "時間割" +
+                       (date.Date.DayOfWeek == dayOfWeek ? "" : $" ({DayOfWeekToStringJp(dayOfWeek)}曜授業)");
             DayJugyos = jugyo;
+        }
+
+        private static string DayOfWeekToStringJp(DayOfWeek dayOfWeek)
+        {
+            // ReSharper disable once SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault
+            return dayOfWeek switch
+            {
+                DayOfWeek.Monday => "月",
+                DayOfWeek.Tuesday => "火",
+                DayOfWeek.Wednesday => "水",
+                DayOfWeek.Thursday => "木",
+                DayOfWeek.Friday => "金",
+                _ => throw new ArgumentException()
+            };
         }
     }
 }
